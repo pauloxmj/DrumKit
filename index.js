3bc95f75f3703b857497b45ced529a8fea9a841c
Paulo Ximenes <65252608+pauloxmj@users.noreply.github.com>
2024-11-08 17:10:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,60 @@
-let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+let tom1 = new Audio("sounds/tom-1.mp3");
+let tom2 = new Audio("sounds/tom-2.mp3");
+let tom3 = new Audio("sounds/tom-3.mp3");
+let tom4 = new Audio("sounds/tom-4.mp3");
+let snare = new Audio("sounds/snare.mp3");
+let crash = new Audio("sounds/crash.mp3");
+let kickBass = new Audio("sounds/kick-bass.mp3");
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-    let buttonInnerHTML = this.innerHTML;
-
-    makeSound(buttonInnerHTML);
-
-    buttonAnimation(buttonInnerHTML);
-
-  });
-
-}
-
-document.addEventListener("keypress", function(event) {
-
-  makeSound(event.key);
-
-  buttonAnimation(event.key);
-
-});
-
-
-function makeSound(key) {
-
+// Assign sound to keys
+function playSound(key) {
   switch (key) {
     case "w":
-      let tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
-
     case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-
     case "s":
-      let tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
-
     case "d":
-      let tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
-
     case "j":
-      let snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
-
     case "k":
-      let crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
-
     case "l":
-      let kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
+      kickBass.play();
       break;
-
-
-    default: console.log(key);
-
+    default:
+      console.log("Invalid key pressed: " + key);
   }
 }
 
-
+// Button effect
 function buttonAnimation(currentKey) {
-
-  let activeButton = document.querySelector("." + currentKey);
-
+  let activeButton = document.querySelector("."+currentKey);
   activeButton.classList.add("pressed");
-
   setTimeout(function() {
     activeButton.classList.remove("pressed");
   }, 100);
-
 }
+
+// Handle button click
+document.querySelectorAll("button").forEach(function(button) {
+  button.addEventListener("click", function() {
+    playSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
+});
+
+// Handle key press
+document.addEventListener("keydown", function(event) {
+  playSound(event.key);
+  buttonAnimation(event.key);
+});
+
